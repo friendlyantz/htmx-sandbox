4d56480334023156d372cc3da136a6593ad5b3d1
friendlyantz <friendlyantz@pm.me>
2023-08-01 13:38:13 +1000

    Rackup app with HTMX

--- rack/lib/app.rb
+++ rack/lib/app.rb
@@ -1,23 +1,23 @@
 
 class App
   def self.call(env)
+      # binding.pry
     case env["REQUEST_METHOD"]
     when "GET"
-      [200, {}, ["Hello World"]]
+      [200, {}, [File.read("lib/index.html")]]
     when "POST"
       request = Rack::Request.new(env)
       body = request.body.read # this can be done once, 'body' gets cleaned after '.read'
       parsed_body = request.media_type.eql?('application/json') ? JSON.parse(body) : body
-      # binding.pry
 
       file_path = 'data_storage'
       File.open(file_path, 'w') { _1.write(parsed_body) }
 
-      [
-        218, 
-        {},
-        ["THIS WAS A POST REQUEST. CONTENT SAVED TO '#{file_path}' FILE"]
-      ]
+      [ 218, {}, [
+        "RESPONSE FROM POST REQUEST, with payload: <#{parsed_body}>", "<br>",
+        "==========================", "<br>",
+        File.read("lib/index.html"),
+      ] ]
     else
       Rack::Response.new('YourDad.zip', 410).finish
     end
