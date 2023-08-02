4e4c41c00593b9676cb4edd24a94afee07a6c3bf
friendlyantz <friendlyantz@pm.me>
2023-08-02 13:06:43 +1000

    WIP HTMX DiceRoll

--- sinatra-diceroll/app.rb
+++ sinatra-diceroll/app.rb
@@ -1,8 +1,33 @@
 require 'sinatra'
+require 'pry'
+
+class Sinatra::Application
+  def initialize
+    game = Game.new
+    @p1 = game.player_one
+    @p2 = game.player_two
+    @dice = rand(1..6).to_s
+
+    super
+  end
+end
 
 set :port, 4568
+
+post '/roll' do
+  binding.pry
+  rand(1..6).to_s
+end
 
 get '/' do
   erb :index
 end
 
+class Game
+  Player = Struct.new(:score)
+  attr_accessor :player_one, :player_two
+  def initialize
+    @player_one = Player.new(score: 0)
+    @player_two = Player.new(score: 0)
+  end
+end
