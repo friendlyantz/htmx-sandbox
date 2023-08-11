454fcebb2eee38db073147424f5a7eb905bb4d69
friendlyantz <friendlyantz@pm.me>
2023-08-12 09:31:10 +1000

    Out-of-band UPD of dice img and stats from 1 HTMX request

--- sinatra-diceroll/app.rb
+++ sinatra-diceroll/app.rb
@@ -3,10 +3,11 @@ require 'pry'
 
 class Sinatra::Application
   def initialize
-    game = Game.new
-    @p1 = game.player_one
-    @p2 = game.player_two
+    @game = Game.new
+    @p1 = @game.player_one
+    @p2 = @game.player_two
     @dice = rand(1..6).to_s
+    @active_player = [@p1, @p2].sample
 
     super
   end
@@ -15,8 +16,17 @@ end
 set :port, 4568
 
 post '/roll' do
-  binding.pry
-  rand(1..6).to_s
+  @dice = rand(1..6)
+  @game.rolled_so_far += @dice
+  @rolled_so_far = @game.rolled_so_far  
+
+  erb :rolled
+end
+
+post '/hold' do
+  # TODO: below is incorrect and just a draft
+  # @active_player.score += @rolled_so_far
+  # reset_rolled_so_far
 end
 
 get '/' do
@@ -24,10 +34,16 @@ get '/' do
 end
 
 class Game
-  Player = Struct.new(:score)
-  attr_accessor :player_one, :player_two
+  Player = Struct.new(:id, :score)
+  attr_accessor :player_one, :player_two, :rolled_so_far
+
   def initialize
-    @player_one = Player.new(score: 0)
-    @player_two = Player.new(score: 0)
+    @player_one = Player.new(id: 1, score: 0)
+    @player_two = Player.new(id: 2, score: 0)
+    reset_rolled_so_far
+  end
+
+  def reset_rolled_so_far
+    @rolled_so_far = 0
   end
 end
