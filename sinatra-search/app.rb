c66f2ffd7c6f28a2d4e3df0d046ef52ddfc8d004
friendlyantz <friendlyantz@pm.me>
2023-08-14 15:53:28 +1000

    Implement HTMX Post to search with Tailwind
    Inspired by https://github.com/johnthethird/sinatra-htmx

--- sinatra-search/app.rb
+++ sinatra-search/app.rb
@@ -1,15 +1,34 @@
 require 'sinatra'
 require 'pry'
+require 'yaml'
 
 class Sinatra::Application
   def initialize
+    @users = 
+      YAML.load_file('db/users.yaml')['results']
+      .map do |u|
+        {
+          name: "#{u['name']['first']} #{u['name']['last']}",
+          email: u['email'],
+          avatar_url: u['picture']['medium']
+        }
+      end.sort_by { _1[:name] }
 
     super
   end
+end
+
+def find_by(name)
+  @users.select{ |u| u[:name] =~ /#{name}/i }
 end
 
 set :port, 4569
 
 get '/' do
   erb :search, layout: :app
+end
+
+post '/q' do
+  @selected_users = params['q'] ? find_by(params['q']) : []
+  erb :_results
 end
